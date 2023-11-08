c9a91d1b973150b53de769daccdb7b759b142d76
sonja-jovic <145637236+sonja-jovic@users.noreply.github.com>
2023-11-07 21:45:26 -0500

    .

--- security.js
+++ security.js
@@ -1,1 +1,24 @@
 console.log("Security js");
+
+const rotated = document.getElementById("menu");
+var menubar = document.getElementById("menu");
+const pages = document.getElementById("menu-content");
+
+
+
+
+if (pages.addEventListener("mouseleave", function () {
+    pages.style.display = "none";
+    rotated.style.transform = "rotate(0deg)";
+}));
+else if (addEventListener("click", function () {
+    rotated.style.transform = "rotate(0deg)";
+    pages.style.display = "none";
+}));
+
+menubar.addEventListener("mouseenter", function () {
+    pages.style.display = "block";
+    rotated.style.transform = "rotate(90deg)";
+    document.getElementById("menu").style.cursor="pointer";
+    /*....*/
+});
