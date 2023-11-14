dfcd15b5de325621a4c11f487f54f198dc109fa7
AndreAzzi05 <aazzi074@uottawa.ca>
2023-11-14 01:21:42 -0500

    Incorporated javascript in menu bar

---
+++ payments.js
@@ -0,0 +1,23 @@
+console.log("payments.js")
+const rotated = document.getElementById("menu");
+var menubar = document.getElementById("menu");
+const pages = document.getElementById("menu-content");
+
+
+
+
+if (pages.addEventListener("mouseleave", function () {
+    pages.style.display = "none";
+    rotated.style.transform = "rotate(0deg)";
+}));
+else if (addEventListener("click", function () {
+    rotated.style.transform = "rotate(0deg)";
+    pages.style.display = "none";
+}));
+
+menubar.addEventListener("mouseenter", function () {
+    pages.style.display = "block";
+    rotated.style.transform = "rotate(90deg)";
+    document.getElementById("menu").style.cursor="pointer";
+    /*....*/
+});
