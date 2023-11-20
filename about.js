152e0d17120dd4e337c3d9a4bc34956ac2a20604
Lourdes Najjar <lourdes.najjar8@gmail.com>
2023-11-19 20:39:48 -0500

    features page

--- about.js
+++ about.js
@@ -47,3 +47,8 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
   }
+
+  $("#clear").click(function(){
+    $(".resul")
+  }
+  )
