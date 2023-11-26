8d17444ee9919cd078442ea92c1f29ce323974fa
sonja-jovic <sonjajovic05@gmail.com>
2023-11-26 14:56:59 -0500

    Fixed top line and menu bar, added text

--- security.js
+++ security.js
@@ -1,38 +1,22 @@
 console.log("Security js");
 
 const rotated = document.getElementById("menu");
-var menubar = document.getElementById("menu");
 const pages = document.getElementById("menu-content");
 let menuopen = false;
 
-document.getElementById("menu").style.cursor = "pointer";
-menubar.addEventListener("click", function () {
-    if (menuopen==true){
-        pages.style.display="none";
-        rotated.style.transform="rotate(0deg)";
-        menuopen=false;
-    }
-    else if (menuopen==false){
+rotated.addEventListener("click", function () {
+    if (menuopen) {
+        pages.style.display = "none";
+        rotated.style.transform = "rotate(0deg)";
+        menuopen = false;
+    } else {
         pages.style.display = "block";
         rotated.style.transform = "rotate(90deg)";
-        menuopen=true;
+        menuopen = true;
     }
 });
 
-
-
-
-if (pages.addEventListener("mouseleave", function () {
+pages.addEventListener("mouseleave", function () {
     pages.style.display = "none";
     rotated.style.transform = "rotate(0deg)";
-})) {
-} else if (addEventListener("click", function () {
-    rotated.style.transform = "rotate(0deg)";
-    pages.style.display = "none";
-}));
-
-menubar.addEventListener("mouseenter", function () {
-    pages.style.display = "block";
-    rotated.style.transform = "rotate(90deg)";
-    document.getElementById("menu").style.cursor = "pointer";
 });
