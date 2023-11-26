7a5c64ec0e483496cd99cb3193c09626906cece4
sonja-jovic <sonjajovic05@gmail.com>
2023-11-26 15:04:29 -0500

    Fixed menu bar

--- security.js
+++ security.js
@@ -1,10 +1,11 @@
 console.log("Security js");
 
 const rotated = document.getElementById("menu");
+const menubar = document.getElementById("menu");
 const pages = document.getElementById("menu-content");
 let menuopen = false;
 
-rotated.addEventListener("click", function () {
+menubar.addEventListener("click", function () {
     if (menuopen) {
         pages.style.display = "none";
         rotated.style.transform = "rotate(0deg)";
@@ -16,7 +17,6 @@ rotated.addEventListener("click", function () {
     }
 });
 
-pages.addEventListener("mouseleave", function () {
-    pages.style.display = "none";
-    rotated.style.transform = "rotate(0deg)";
+menubar.addEventListener("mouseenter", function () {
+    document.getElementById("menu").style.cursor = "pointer";
 });
